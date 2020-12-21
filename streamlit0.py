8fbf4f9a26fbf60839f7d144de5075c543db5d14
EvandroManso <76075140+EvandroManso@users.noreply.github.com>
2020-12-21 17:25:01 -0300

    Update streamlit0.py

--- streamlit0.py
+++ streamlit0.py
@@ -28,7 +28,7 @@ df = pd.DataFrame({
 #muito facil exibir dataframes:
 df
 
-df_novo = pd.read_excel ("dados3.xlsx", sheet_name="Sheet1", header=0)#, engine='openpyxl')
+df_novo = pd.read_csv("dados3.csv", header=0)#, engine='openpyxl')
 df_novo
 ## plotar grafico (sem bokeh)
 chart_data = pd.DataFrame(
